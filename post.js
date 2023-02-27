8836504d26babac99fa984ec162a38424c826088
Tony Rasa <trasa@meancat.com>
2023-02-27 11:41:01 -0800

    adding in axios for http requests

--- post.js
+++ post.js
@@ -1,3 +1,7 @@
+
+import axios from 'axios';
+
+
 // chalk prints pretty colors
 import chalk from 'chalk';
 const log = console.log;
@@ -16,10 +20,12 @@ import fs from 'fs/promises';
 // some minor improvement in safety. Note that this isn't
 // 100% safe...don't run untrusted scripts...
 import {NodeVM} from 'vm2'; 
+
 // sandbox is the state that exists between runs
 const sandbox = {};
 sandbox.pm = {};
 sandbox.pm.environment = new Map();
+
 const vm = new NodeVM({ 
     console: 'inherit',
     sandbox: sandbox,
@@ -36,8 +42,7 @@ program
     .action((filename, options) => {
         log(`processing ${filename}`);
         fs.readFile(filename)
-        .then(async data=> {
-            log('data read');
+        .then(async data => {
             let postObj = JSON.parse(data);
             log(`Loaded file ${chalk.green(postObj.info.name)}`);
             await doRun(postObj);
@@ -67,12 +72,28 @@ program
 
 // given a postman json object, run all the tests in it
 function doRun(postObj) {
+    doVariable(postObj);
+    
     postObj.item.forEach(folder => {
         log(`Folder: ${folder.name}`);
         folder.item.forEach(item => {
+            log(`Item: ${item.name}`);
             doPreRequestEvent(folder, item);
+            
+            doRequest(folder, item);
         });
     });
+}
+
+// process the 'variable' section, loading some values
+// into our sandbox global space
+function doVariable(postObj) {
+    postObj.variable.forEach(kv => {
+        sandbox.pm.environment.set(kv.key, kv.value);
+    });
+    // TODO overrides from command line options here
+    
+    console.log(chalk.blue("Variables processed"));
 }
 
 // execute all the 'prerequest' events found in this item
@@ -83,10 +104,33 @@ function doPreRequestEvent(folder, item) {
             return;
         }
         let script = event.script.exec.join('\n');
-        console.log(chalk.yellow(script));
-        vm.run(script, "s.js");
-        log(error(sandbox.pm.environment.get('testUser')));
+        if (script.length > 0) {
+            console.log(chalk.yellow("PreRequestEvent Script:"))
+            console.log(chalk.yellow(script));    
+            console.log(chalk.yellow("--------"));
+            vm.run(script);
+        }
     });
+}
+
+// run an HTTP Request against a target, in a sandbox
+// keep track of all the results for testing later!
+async function doRequest(folder, item) {
+
+    let req = item.request;
+    if (req.method.toUpperCase() === "POST") {
+        await doRequestPost(folder, item, req);
+    } else if (req.method.toUpperCase() === "GET") {
+        await doRequestGet(folder, item, req);
+    } else {
+        // else other types...
+        log(error(`Unhandled request type: ${req.method}`));
+    }
+
+    // method
+    // header
+    // body 
+    // url 
 }
 
 program.parse();
