5107895b3393aaa96754a68507fde7ae8afb2d0e
Tony Rasa <trasa@meancat.com>
2023-02-27 11:02:26 -0800

    got VM2 working with some simple scripts

--- post.js
+++ post.js
@@ -1,14 +1,30 @@
+// chalk prints pretty colors
 import chalk from 'chalk';
 const log = console.log;
 chalk.level = 1;
 const error = chalk.redBright;
 const warning = chalk.yellow;
 
+// commander does our command-line interface
 import {Command} from 'commander';
 const program = new Command();
 
+// file system work with promises
 import fs from 'fs/promises';
 
+// execute all our scripted code in a VM for at least
+// some minor improvement in safety. Note that this isn't
+// 100% safe...don't run untrusted scripts...
+import {NodeVM} from 'vm2'; 
+// sandbox is the state that exists between runs
+const sandbox = {};
+sandbox.pm = {};
+sandbox.pm.environment = new Map();
+const vm = new NodeVM({ 
+    console: 'inherit',
+    sandbox: sandbox,
+    require: { external: true, root: './'}
+});
 
 program
     .name('post')
@@ -31,14 +47,46 @@ program
         });
     });
 
-async function doRun(postObj) {
+    // just debugging stuff
+    program
+        .command('try')
+        .action(async (options)=> {
+
+            let script = `
+            var digits = '' + Math.floor((Math.random() * 100000));
+            console.log('digits ' + digits);
+            pm.environment.set("testUser", "user" + digits);
+            pm.environment.set("testPassword", "password" + digits);
+            pm.environment.set("testAvatar", "avi" + digits);
+            return pm;
+            `;
+            await vm.run(script, 's.js');
+            log(error(JSON.stringify(sandbox)));
+            log(sandbox.pm.environment.get('testUser'));
+        });
+
+// given a postman json object, run all the tests in it
+function doRun(postObj) {
     postObj.item.forEach(folder => {
         log(`Folder: ${folder.name}`);
         folder.item.forEach(item => {
-            log(`Item: ${item.name}`);
+            doPreRequestEvent(folder, item);
         });
     });
-}    
+}
 
+// execute all the 'prerequest' events found in this item
+function doPreRequestEvent(folder, item) {
+    item.event.forEach(event => {
+        if (event.listen != "prerequest") {
+            // not the event type we're looking for
+            return;
+        }
+        let script = event.script.exec.join('\n');
+        console.log(chalk.yellow(script));
+        vm.run(script, "s.js");
+        log(error(sandbox.pm.environment.get('testUser')));
+    });
+}
 
 program.parse();
