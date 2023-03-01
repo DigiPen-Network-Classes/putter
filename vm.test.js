3d3c7138202da18e042b39a6add3abe3018d6d40
Tony Rasa <trasa@meancat.com>
2023-02-28 20:30:05 -0800

    cleaning up test

--- vm.test.js
+++ vm.test.js
@@ -1,23 +1,10 @@
 import { createVM } from './vm.js'
 
 test('timeout test', async ()=> {
-    let vm = createVM();
-    console.log("start");
-            //let x = "setTimeout(()=> { console.log('timeout happened')}, 5000);";
-//            let x = "await new Promise(resolve => setTimeout(resolve, 5000));"
-            //await sandbox.sleep(1000);
-            //let result = await vm.run("(async () => { await sleep(5000); })()");
-            //
-//            let x = "for (let i=0; i < 100000; i++) { if (i % 100 === 0) console.log(i); }";
-//            let result = await vm.run("(async () => { await sleep(5000); })()");
-
-//            let x = `let waitTill = new Date(new Date().getTime() + 10 * 1000);
-//            while (waitTill > new Date()) {}`;
-
-            let x = `sleep(10000)`;
-
-            let result = await vm.run(x);
-            console.log(result);
-            console.log("done.")
+        let vm = createVM();
+        console.log("start");
+        let x = `console.log('feeling sleepy'); sleep(500); console.log('finished sleeping');`;
+        await vm.run(x);
+        console.log("done.")
 });
 
