f7468d4c15d89eabfa459103026d126ebe3209e9
Tony Rasa <trasa@meancat.com>
2023-02-28 20:30:29 -0800

    removing empty commander handler for testing vm sleep

--- post.js
+++ post.js
@@ -45,29 +45,6 @@ program
         });
     });
 
-    program
-        .command('donothing')
-        .action(async () => {
-            console.log("start");
-            let vm = createVM();
-            //let x = "setTimeout(()=> { console.log('timeout happened')}, 5000);";
-//            let x = "await new Promise(resolve => setTimeout(resolve, 5000));"
-            //await sandbox.sleep(1000);
-            //let result = await vm.run("(async () => { await sleep(5000); })()");
-            //
-//            let x = "for (let i=0; i < 100000; i++) { if (i % 100 === 0) console.log(i); }";
-//            let result = await vm.run("(async () => { await sleep(5000); })()");
-
-//            let x = `let waitTill = new Date(new Date().getTime() + 10 * 1000);
-//            while (waitTill > new Date()) {}`;
-
-            let x = `sleep(10000)`;
-
-            let result = await vm.run(x);
-            console.log(result);
-            console.log("done.")
-        });
-
 // given a postman json object, run all the tests in it
 async function doRun(postObj) {
     loadVariables(postObj);
